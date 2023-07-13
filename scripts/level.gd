ee36fb9fc538654dfa094ffdb3a0a87998fde8eb
Alan <alanhaugen@gmail.com>
2023-07-13 23:29:29 +0200

    trying to make a main loop

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+var score
+
+func new_game():
+	score = 0
+	$Timer.start()
+
+#func _on_mob_timer_timeout():
+	# Create a new instance
+
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+func _ready():
+	new_game()
